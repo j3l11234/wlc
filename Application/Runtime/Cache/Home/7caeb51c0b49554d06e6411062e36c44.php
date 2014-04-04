565a85cad31e82434e7e4779288bffa6e82505cd
j3l11234 <297259024@qq.com>
2014-04-04 21:36:38 +0800

    增加滑动动画 删除临时文件

--- Application/Runtime/Cache/Home/7caeb51c0b49554d06e6411062e36c44.php
+++
@@ -1,7 +0,0 @@
-<?php if (!defined('THINK_PATH')) exit(); echo ($attendWidget); ?>
-
-<script type="text/javascript">
-	$(function(){
-		setCookie("page","home-index");
-	});	
-</script>
--- Application/Runtime/Cache/Home/dc2670527f5b3d6f836127901fddc75d.php
+++
@@ -1,9 +0,0 @@
-<?php if (!defined('THINK_PATH')) exit();?> 
-
-<?php echo ($queryHtml); ?>
-
-<script type="text/javascript">
-	$(function(){
-		setCookie("page","home-attend");
-	});	
-</script>
